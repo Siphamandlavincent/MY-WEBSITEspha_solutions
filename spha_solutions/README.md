d281963805937ce3fed868e32bc67dc872cbeaac
Siphamandlavincent <v.khumalo@yahoo.com>
2024-11-21 09:17:22 +0200

    Initial commit

---
+++ spha_solutions/README.md
@@ -0,0 +1,4 @@
+# SPHA_SOLUTIONS
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/siyanda-pitsi/pen/RwXQQRM](https://codepen.io/siyanda-pitsi/pen/RwXQQRM).
+
---
+++ spha_solutions/src/script.js
@@ -0,0 +1,6 @@
+<script>
+    function toggleInfo() {
+        const popup = document.getElementById("infoPopup");
+        popup.style.display = popup.style.display === "none" ? "block" : "none";
+    }
+</script>
